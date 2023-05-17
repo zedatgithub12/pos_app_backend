b1af17273ba68053429f94c2bf1d4b58b4a2a485
zedatgithub12 <96112790+zedatgithub12@users.noreply.github.com>
2023-05-17 18:22:48 +0300

    update on may 17

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
